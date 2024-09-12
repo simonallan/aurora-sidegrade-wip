5852b46873ec9c0f475610634187f8885b3a51ae
Simon Allan <simon.allan@cancer.org.uk>
2024-09-12 12:58:03 +0100

    create aurora db instance

--- bin/ofr-aurora.ts
+++ bin/ofr-aurora.ts
@@ -1,37 +1,63 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
+import { App, Aws } from "aws-cdk-lib";
 import { AuroraStack } from "../lib/ofr-aurora-stack";
-import { LandingZoneAccountType } from "../lib/network";
+import {
+  LandingZoneAccountType,
+  // DEVELOPMENT_ENVIRONMENT_NAME,
+  // INTEGRATION_ENVIRONMENT_NAME,
+} from "../lib/network";
 
 const environments: LandingZoneAccountType[] = [
-  LandingZoneAccountType.DEV,
   LandingZoneAccountType.INT,
-  LandingZoneAccountType.PROD,
+  // LandingZoneAccountType.PROD, // Disable that shiz
 ];
 
-const app = new cdk.App();
+const app = new App();
 
 const env = {
-  account: "651948078005",
+  account:
+    process.env.CDK_DEPLOY_ACCOUNT ||
+    process.env.CDK_DEFAULT_ACCOUNT ||
+    Aws.ACCOUNT_ID,
   region: "eu-west-2",
 };
 
+// No Prod deploys
+if (!["651948078005"].includes(env.account)) {
+  throw new Error(
+    'Please ensure this script is being used with the following AWS account: "651948078005": Integration'
+  );
+}
+
+// if (
+//   process.env.ENVIRONMENT_NAME !== "development" &&
+//   process.env.ENVIRONMENT_NAME !== "integration"
+// )
+//   throw new Error(
+//     "Environment variable ENVIRONMENT_NAME must be defined. Please use `export ENVIRONMENT_NAME=(development/integration)`"
+//   );
+// const environment: string = process.env.ENVIRONMENT_NAME;
+
 environments.forEach((landingZoneAccountType: LandingZoneAccountType) => {
-  const auroraStack = new AuroraStack(app, `aurora-stack-${landingZoneAccountType}`, {
-    env,
-    description: "Aurora :: WIP :: DB Sidegrade",
-    landingZoneAccountType,
-    terminationProtection: true,
-    commitId: process.env.COMMIT_ID,
-    tags: {
-      "Cost-Centre": "TC7003",
-      "Sub-Project-Code": "SO00002-0000",
-      Product: "Online Fundraising",
-      Environment: landingZoneAccountType,
-      "Support-Level": "2",
-      Name: `Stack: ofr-admin-application-${landingZoneAccountType}`,
-      id: `ofr-admin-application-${landingZoneAccountType}`,
-    },
-  });
+  const auroraStack = new AuroraStack(
+    app,
+    `poc-aurora-${landingZoneAccountType}`,
+    {
+      description: "Aurora :: WIP :: DB Sidegrade",
+      env,
+      landingZoneAccountType,
+      // environment,
+      terminationProtection: true,
+      tags: {
+        Product: "Online Fundraising",
+        Environment: landingZoneAccountType,
+        "Cost-Centre": "TC7003",
+        "Sub-Project-Code": "SO00002-0000",
+        "Support-Level": "0",
+        Name: `Stack: poc-aurora-${landingZoneAccountType}`,
+        id: `poc-aurora-${landingZoneAccountType}`,
+      },
+    }
+  );
 });
--- lib/ofr-aurora-stack.ts
+++ lib/ofr-aurora-stack.ts
@@ -1,12 +1,17 @@
-import { Stack, StackProps, Duration, RemovalPolicy, CfnOutput } from "aws-cdk-lib";
+import {
+  Stack,
+  StackProps,
+  Duration,
+  RemovalPolicy,
+  CfnOutput,
+  Tags,
+} from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { getVpcId, getVpcSubnets, LandingZoneAccountType } from "./network";
 import { Vpc, SubnetType } from "aws-cdk-lib/aws-ec2";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import {
-  ServerlessCluster,
   AuroraMysqlEngineVersion,
-  AuroraCapacityUnit,
   DatabaseCluster,
   DatabaseClusterEngine,
   ClusterInstance,
@@ -17,15 +22,24 @@ import { Credentials } from "aws-cdk-lib/aws-rds";
 
 export interface AuroraStackProps extends StackProps {
   readonly landingZoneAccountType: LandingZoneAccountType;
-  readonly commitId?: string;
+  // readonly environment: string;
 }
 
 export class AuroraStack extends Stack {
   constructor(scope: Construct, id: string, props: AuroraStackProps) {
     super(scope, id, props);
 
+    Object.entries(Tags).forEach(([name, value]) => {
+      Tags.of(this).add(name, value);
+    });
+
     // `getVpcSubnets()` must only be called once per subnet type
-    const isolatedSubnets = getVpcSubnets(this, id, SubnetType.PRIVATE_ISOLATED, props.landingZoneAccountType);
+    const isolatedSubnets = getVpcSubnets(
+      this,
+      id,
+      SubnetType.PRIVATE_ISOLATED,
+      props.landingZoneAccountType
+    );
 
     // Vpc.
     const vpc = Vpc.fromLookup(this, `${id}-vpc`, {
@@ -33,15 +47,19 @@ export class AuroraStack extends Stack {
     });
 
     // Database
-    const databasePasswordSecret = new Secret(this, `${id}-database-password`, {
-      description: `Aurora :: WIP :: ${props.landingZoneAccountType} :: Database password`,
-      generateSecretString: {
-        excludePunctuation: true,
-        excludeCharacters: `!@#$%^&*/"`,
-        includeSpace: false,
-        passwordLength: 32,
-      },
-    });
+    const databasePasswordSecret = new Secret(
+      this,
+      `${id}-wip-database-password`,
+      {
+        description: `WIP :: Aurora :: ${props.landingZoneAccountType} :: Database password`,
+        generateSecretString: {
+          excludePunctuation: true,
+          excludeCharacters: `!@#$%^&*/"`,
+          includeSpace: false,
+          passwordLength: 32,
+        },
+      }
+    );
 
     /**
      * Let's try a serverless cluster from SnapShot, same settings as below:
@@ -67,27 +85,40 @@ export class AuroraStack extends Stack {
     //   },
     // });
 
-    // // New DB Aurora MySQL provisioned instance as Aurora Serverless V1 is going EoL
-    const databaseInstance = new DatabaseCluster(this, `${id}-aurora-provisioned`, {
-      clusterIdentifier: `${id}-aurora-provisioned`,
-      engine: DatabaseClusterEngine.auroraMysql({ version: AuroraMysqlEngineVersion.VER_2_11_4 }),
-      writer: ClusterInstance.provisioned(`${id}-rds-provisioned-writer`, {
-        // This cert will need updating to `CaCertificate.RDS_CA_RSA2048_G1` by August 22 2024
-        caCertificate: CaCertificate.RDS_CA_2019,
-        instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.SMALL),
-      }),
-      credentials: Credentials.fromPassword("drupal", databasePasswordSecret.secretValue),
-      vpc,
-      vpcSubnets: {
-        subnets: isolatedSubnets,
-      },
-      removalPolicy:
-        props.landingZoneAccountType === LandingZoneAccountType.PROD ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
-      backup: {
-        retention: props.landingZoneAccountType === LandingZoneAccountType.PROD ? Duration.days(30) : Duration.days(1),
-        preferredWindow: "04:30-05:00",
-      },
-    });
+    // New DB Aurora MySQL provisioned instance as Aurora Serverless V1 is going EoL
+    const databaseInstance = new DatabaseCluster(
+      this,
+      `${id}-provisioned-db-cluster`,
+      {
+        clusterIdentifier: `${id}-cluster`,
+        engine: DatabaseClusterEngine.auroraMysql({
+          version: AuroraMysqlEngineVersion.VER_2_11_4,
+        }),
+        writer: ClusterInstance.provisioned(`${id}-writer`, {
+          caCertificate: CaCertificate.RDS_CA_RSA2048_G1,
+          instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.SMALL),
+        }),
+        credentials: Credentials.fromPassword(
+          "drupal",
+          databasePasswordSecret.secretValue
+        ),
+        vpc,
+        vpcSubnets: {
+          subnets: isolatedSubnets,
+        },
+        removalPolicy:
+          props.landingZoneAccountType === LandingZoneAccountType.PROD
+            ? RemovalPolicy.RETAIN
+            : RemovalPolicy.DESTROY,
+        backup: {
+          retention:
+            props.landingZoneAccountType === LandingZoneAccountType.PROD
+              ? Duration.days(30)
+              : Duration.days(1),
+          preferredWindow: "04:30-05:00",
+        },
+      }
+    );
 
     new CfnOutput(this, `${id} Aurora-Provisioned Endpoint`, {
       value: databaseInstance.clusterEndpoint.hostname,
